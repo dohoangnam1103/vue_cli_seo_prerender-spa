b14a96e89e285c0d50e041c528e0d37f177eec6e
nam.d <nam.d@gmail.com>
2021-08-02 10:28:41 +0700

    add title about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page test SEO crawl nnnnnnn</h1>
     <div>label from config store {{ labelAboutPage }}</div>
     <div>
       <ul>
